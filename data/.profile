3b1a5f089dd235f67ece0ef59fbeb0559b78b4b4
BUGTree1 <szymek2017r@gmail.com>
2026-04-02 10:08:54 +0200

    script

---
+++ data/.profile
@@ -0,0 +1,22 @@
+#
+# ~/.profile
+#
+
+source /usr/share/bash-completion/bash_completion
+
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+alias kkill='qdbus org.kde.KWin /KWin killWindow'
+alias listx='xlsclients -l'
+alias restart_plasma='systemctl restart --user plasma-plasmashell'
+alias kde_sudo='pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY KDE_SESSION_VERSION=5 KDE_FULL_SESSION=true'
+alias auto_rebuild='yay -S --rebuild $(checkrebuild | cut -c 8-)'
+
+#PS1='[\u@\h \W]\$ '
+PS1='\[\e[38;5;27m\]\u\[\e[38;5;39m\]@\[\e[96m\]\h\[\e[38;5;33m\]:\[\e[38;5;159m\]\w\[\e[38;5;195m\]\$\[\e[0m\] '
+
+export PATH="$PATH:/home/szyme/.local/bin"
+
+bind 'set show-all-if-ambiguous on'
+bind 'TAB:menu-complete'
+
---
+++ symlink.sh
@@ -0,0 +1,37 @@
+#/usr/bin/env bash
+
+set -xe
+
+#\/ remember that the prefixes must end with /
+FILE_REAL_PREFIX="${HOME}/"
+FILE_SYMLINK_PREFIX="${PWD}/data/"
+
+declare -a files=(".config/zed/settings.json"
+".config/alacritty/alacritty.toml"
+".config/plasmashellrc"
+".bash_profile"
+".bashrc"
+".languagetool.cfg"
+".nanorc"
+".nvidia-settings-rc"
+".profile"
+".xinitrc"
+".Xresources")
+
+for i in "${!files[@]}"
+do
+    file_path="${FILE_REAL_PREFIX}${files[${i}]}"
+    symlink_path="${FILE_SYMLINK_PREFIX}${files[${i}]}"
+    if [ -f "${file_path}" ] && [ ! -L "${file_path}" ]; then
+        mkdir -p "$(dirname "${symlink_path}")"
+        mv -T "${file_path}" "${symlink_path}"
+        ln -sf -T "${symlink_path}" "${file_path}"
+    else
+        if [ -f "${symlink_path}" ]; then
+            mkdir -p "$(dirname "${file_path}")"
+            ln -sf -T "${symlink_path}" "${file_path}"
+        else
+            echo "ERROR: The file \"${file_path}\" and its symlink \"${symlink_path}\" does not exist!"
+        fi
+    fi
+done
