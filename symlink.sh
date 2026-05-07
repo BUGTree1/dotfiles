1511bafe507be624478b197841fb4576b47c5e75
BUGTree1 <szymek2017r@gmail.com>
2026-05-07 19:56:57 +0200

    update

--- symlink.sh
+++ symlink.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
-set -xe
+set -e
 
-#\/ remember that the prefixes must end with /
-FILE_REAL_PREFIX="${HOME}/"
-FILE_SYMLINK_PREFIX="${PWD}/data/"
+SCRIPT_DIR=$(dirname "$(realpath "${0}")")
 
-declare -a files=(".config/zed/settings.json"
+# \/ remember that the prefixes must end with /
+SYMLINK_REAL_PREFIX="/"
+SYMLINK_REAL_HOME_PREFIX="${HOME}/"
+SYMLINK_STORE_PREFIX="${SCRIPT_DIR}/data/"
+SYMLINK_STORE_HOME_PREFIX="${SCRIPT_DIR}/data/home/"
+
+declare -a files=(
+)
+
+declare -a home_files=(
+".config/zed/settings.json"
 ".config/alacritty/alacritty.toml"
 ".config/alacritty/github_dark_high_contrast.toml"
 ".config/plasmashellrc"
@@ -17,22 +25,69 @@ declare -a files=(".config/zed/settings.json"
 ".nvidia-settings-rc"
 ".profile"
 ".xinitrc"
-".Xresources")
+".Xresources"
+)
+
+# \/ remember that the folders must not end with / or /*
+declare -a folders=(
+)
+
+declare -a home_folders=(
+".nano"
+)
+
+symlink_path_ex () {
+    real_path=${1}
+    symlink_path=${2}
+    folder=${3}
+    if [ -e "${real_path}" ] && [ ! -L "${real_path}" ]; then
+        mkdir -p "$(dirname "${symlink_path}")"
+        if [ "${folder}" = '/*' ]; then  mkdir -p "${symlink_path}"; fi
+        mv $( ls "${real_path}"${folder} 2>/dev/null ) "${symlink_path}" 2>/dev/null | :
+        rmdir "${real_path}"
+        ln -sf -T "${symlink_path}" "${real_path}"
+        echo "Moved and Linked: \"${real_path}\" to: \"${symlink_path}\""
+    else
+        if [ -e "${symlink_path}" ]; then
+            mkdir -p "$(dirname "${real_path}")"
+            if [ "${folder}" = '/*' ]; then  mkdir -p "${real_path}"; fi
+            ln -sf -T "${symlink_path}" "${real_path}"
+            echo "Linked: \"${real_path}\" to: \"${symlink_path}\""
+        else
+            echo "ERROR: The path \"${real_path}\" and its symlink \"${symlink_path}\" do not exist!"
+            exit 1
+        fi
+    fi
+}
 
 for i in "${!files[@]}"
 do
-    file_path="${FILE_REAL_PREFIX}${files[${i}]}"
-    symlink_path="${FILE_SYMLINK_PREFIX}${files[${i}]}"
-    if [ -f "${file_path}" ] && [ ! -L "${file_path}" ]; then
-        mkdir -p "$(dirname "${symlink_path}")"
-        mv -T "${file_path}" "${symlink_path}"
-        ln -sf -T "${symlink_path}" "${file_path}"
-    else
-        if [ -f "${symlink_path}" ]; then
-            mkdir -p "$(dirname "${file_path}")"
-            ln -sf -T "${symlink_path}" "${file_path}"
-        else
-            echo "ERROR: The file \"${file_path}\" and its symlink \"${symlink_path}\" does not exist!"
-        fi
-    fi
+    curr_file="${files[${i}]}"
+    file_path="${SYMLINK_REAL_PREFIX}${curr_file}"
+    symlink_path="${SYMLINK_STORE_PREFIX}${curr_file}"
+    symlink_path_ex "${file_path}" "${symlink_path}" ''
+done
+
+for i in "${!home_files[@]}"
+do
+    curr_file="${home_files[${i}]}"
+    file_path="${SYMLINK_REAL_HOME_PREFIX}${curr_file}"
+    symlink_path="${SYMLINK_STORE_HOME_PREFIX}${curr_file}"
+    symlink_path_ex "${file_path}" "${symlink_path}" ''
+done
+
+for i in "${!folders[@]}"
+do
+    curr_folder="${folders[${i}]}"
+    folder_path="${SYMLINK_REAL_PREFIX}${curr_folder}"
+    symlink_path="${SYMLINK_STORE_PREFIX}${curr_folder}"
+    symlink_path_ex "${folder_path}" "${symlink_path}" '/*'
+done
+
+for i in "${!home_folders[@]}"
+do
+    curr_folder="${home_folders[${i}]}"
+    folder_path="${SYMLINK_REAL_HOME_PREFIX}${curr_folder}"
+    symlink_path="${SYMLINK_STORE_HOME_PREFIX}${curr_folder}"
+    symlink_path_ex "${folder_path}" "${symlink_path}" '/*'
 done
